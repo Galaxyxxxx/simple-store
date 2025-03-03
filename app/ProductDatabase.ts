d334debc0aa70f4c2a9d7b2659e17bc33cd98ef2
Wojciech <prokopw581@gmail.com>
2025-03-03 14:10:57 +0000

    beginning of UI

---
+++ app/ProductDatabase.ts
@@ -0,0 +1,53 @@
+import Store from "@/types/store";
+
+const products: Store.Product[] = [
+    {
+        id: 1,
+        title: "Samsung Galaxy S21 Ultra",
+        price: 299,
+        description: "lim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.",
+        category: "Electronics",
+        image: "/product1.jpg",
+        rating: {
+            rate: 7.5,
+            count: 257
+        },
+    },
+    {
+        id: 2,
+        title: "Samsung TV 55U",
+        price: 1500,
+        description: "Zostaw telefon w kieszeni i wygodnie steruj słuchawkami za pomocą prostych gestów. Nawet przy szybkim przesunięciu, możesz mieć pewność, że Galaxy Buds3 pozostaną bezpiecznie w Twoich uszach.",
+        category: "Electronics",
+        image: "/product2.jpg",
+        rating: {
+            rate: 10,
+            count: 1560
+        },
+    },
+    {
+        id: 3,
+        title: "Samsung Smartfridge Nofrozr",
+        price: 360,
+        description: "Zostaw telefon w kieszeni i wygodnie steruj słuchawkami za pomocą prostych gestów. Nawet przy szybkim przesunięciu, możesz mieć pewność, że Galaxy Buds3 pozostaną bezpiecznie w Twoich uszach.",
+        category: "Electronics",
+        image: "/product3.jpg",
+        rating: {
+            rate: 1,
+            count: 1
+        },
+    },
+    {
+        id: 4,
+        title: "Samsung Headphones WH1000XM3",
+        price: 55,
+        description: "NZostaw telefon w kieszeni i wygodnie steruj słuchawkami za pomocą prostych gestów. Nawet przy szybkim przesunięciu, możesz mieć pewność, że Galaxy Buds3 pozostaną bezpiecznie w Twoich uszach.",
+        category: "Electronics",
+        image: "/product4.jpg",
+        rating: {
+            rate: 8.0,
+            count: 1000
+        },
+    }];
+
+export default products;
--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -1,4 +1,8 @@
 import Store from "@/types/store";
+import {Card, CardContent, CardDescription, CardTitle, CardHeader, CardFooter} from "@/components/ui/card"
+import { Button } from "@/components/ui/button"
+import { Plus, Minus } from "lucide-react";
+import Image from "next/image";
 
 interface ProductCardProps {
   product: Store.Product;
@@ -8,5 +12,31 @@ interface ProductCardProps {
 
 export default function ProductCard({product}: ProductCardProps) {
 
-  return <></>
+  return <Card>
+    <CardHeader className="flex-row place-content-between">
+      <div className="flex-col">
+        <CardTitle>{product.title}</CardTitle>
+        <CardDescription>Category: {product.category}</CardDescription>
+      </div>
+      <div className="flex-row">
+        <Button><Plus /></Button>
+        
+        <Button><Minus /></Button>
+      </div>
+    </CardHeader>
+    <CardContent className="flex stretch-items">
+      <Image src={product.image} alt={product.title} width={200} height={200} />
+      <CardDescription>{product.description}</CardDescription>
+    </CardContent>
+    <CardFooter className="flex place-content-between">
+      <div className="flex-col"> 
+        <p>Rating: {product.rating.rate}</p> 
+        <p>({product.rating.count} review/s)</p>  
+      </div>
+      <div className="grid justify-items-end">
+        <p>Price: ${product.price}</p>
+        <Button>Add to Cart</Button>
+      </div>
+    </CardFooter>
+  </Card>
 }
