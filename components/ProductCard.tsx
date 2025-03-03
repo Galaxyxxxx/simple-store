7606ea7a6cc0f1eacd19808e60450b6fc83fd6d5
Wojciech <prokopw581@gmail.com>
2025-03-03 11:13:39 +0000

    .

---
+++ components/ProductCard.tsx
@@ -0,0 +1,12 @@
+import Store from "@/types/store";
+
+interface ProductCardProps {
+  product: Store.Product;
+  howManySelected?: number;
+}
+
+
+export default function ProductCard({product}: ProductCardProps) {
+
+  return <></>
+}
---
+++ types/store.ts
@@ -0,0 +1,18 @@
+declare namespace Store {
+  interface Product {
+    id: number,
+    title: string,
+    price: number,
+    description: string,
+    category: string,
+    image: string,
+    rating: {
+        rate: number,
+        count: number
+    }
+  }
+
+  
+}
+
+export default Store;
